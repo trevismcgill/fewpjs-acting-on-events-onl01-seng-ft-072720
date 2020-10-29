f21a6c7d3270fe952a4e975163c3d5535766329f
trevismcgill <trevismcgill25@gmail.com>
2020-10-29 13:54:27 -0500

    commit tree f8f4b61a434b3f0ccf67b4eee37aa2e053a833e2

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+let dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4";
+
+function moveDodgerLeft() {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+   
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    var leftNumbers = dodger.style.left.replace('px', '');
+    var left = parseInt(leftNumbers, 10)
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`
+    }
+  }
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+    else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
